761863f9fc80660535e62285e2065cf8270c59e4
hseniht <thinesh_kumar_mu@hotmail.com>
2020-03-08 14:57:22 +0800

    Updates

---
+++ precache-manifest.9bff5536941aabfd4812c04479c59b09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d820bef0ea936fce7b5fa5ff0808fa06",
+    "url": "/awesome-app-1/index.html"
+  },
+  {
+    "revision": "3e6894679ec916b8d40f",
+    "url": "/awesome-app-1/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "755cbb5623cc8319a180",
+    "url": "/awesome-app-1/static/css/main.ddca8c7d.chunk.css"
+  },
+  {
+    "revision": "3e6894679ec916b8d40f",
+    "url": "/awesome-app-1/static/js/2.eb07d0c2.chunk.js"
+  },
+  {
+    "revision": "41f2ae5be27a63113cb964beb60661da",
+    "url": "/awesome-app-1/static/js/2.eb07d0c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "755cbb5623cc8319a180",
+    "url": "/awesome-app-1/static/js/main.9aa8fd5e.chunk.js"
+  },
+  {
+    "revision": "c5ca2d463ef2bec40ece",
+    "url": "/awesome-app-1/static/js/runtime-main.818243d3.js"
+  },
+  {
+    "revision": "044792f5985c5f18a692bd65d6cdab6a",
+    "url": "/awesome-app-1/static/media/minimalist-abstract-hexagon-colorful-a1949.044792f5.jpg"
+  }
+]);
