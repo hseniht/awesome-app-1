4462bcf05c10b907ff50e00a2e8b3ab634416349
hseniht <thinesh_kumar_mu@hotmail.com>
2020-05-01 14:18:04 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/awesome-app-1/precache-manifest.12af9b223ca5b978366e34bce7a40fa3.js"
+  "/awesome-app-1/precache-manifest.ea824bce1cedb9799b0c653aa19c97f7.js"
 );
 
 self.addEventListener('message', (event) => {
