5b88f617ea7d4c8f26e1171b4f1a0158000f1454
hseniht <thinesh_kumar_mu@hotmail.com>
2020-05-03 13:27:18 +0800

    *Use redux-firestore way to get data to redux store - add new reducer, firestoreReducer to sync data to our redux store - add firestoreConnect to connect to a component to sync the particular collection from firebase (read on the documentation of react-redux-firebase for more info/clarification)

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../project/ProjectList'
 import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 class Dashboard extends Component {
    render() {
@@ -23,9 +25,21 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
+   console.log("tk state", state);
    return {
-      projects: state.project.projects
+      // projects: state.project.projects
+      projects: state.firestore.ordered.projects
    }
 }
 
-export default connect(mapStateToProps)(Dashboard)
+//Add diff HOC's together using 'compose'
+//HOC 1: `connect` from redux,
+//HOC 2: `firestoreConnect` from react-redux-firebase.
+export default compose(
+   connect(mapStateToProps),
+   firestoreConnect(() => ['projects']) // sync the collection from Firestore into redux
+   // alt way:
+   // firestoreConnect([
+   //    { collection: 'projects' }
+   // ])
+)(Dashboard)
