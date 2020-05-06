ba65e94b2871acbc810a8f5e23a370bb4313c729
hseniht <thinesh_kumar_mu@hotmail.com>
2020-05-06 23:27:34 +0800

    Project details - get each data from firestore - get the data collection from firebase's firestore to our redux using the package

--- src/components/project/ProjectDetails.js
+++ src/components/project/ProjectDetails.js
@@ -1,22 +1,49 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 const ProjectDetails = (props) => {
-   const id = props.match.params.id;
-
-   return (
-      <div className="container section project-details">
-         <div className="card z-depth-0">
-            <div className="card-content">
-               <span className="card-title">Project Title - {id}</span>
-               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero modi deserunt iste facere repudiandae! Molestias impedit eius nam doloribus rerum nulla expedita. Deserunt saepe error, quaerat laudantium qui magni!</p>
-            </div>
-            <div className="card-action grey lighten-4 grey-text">
-               <div>Posted by Professor Oak</div>
-               <div>2nd September, 2am</div>
+   const { project } = props;
+   if (project) {
+      return (
+         <div className="container section project-details">
+            <div className="card z-depth-0">
+               <div className="card-content">
+                  <span className="card-title">{project.title}</span>
+                  <p>{project.content}</p>
+               </div>
+               <div className="card-action grey lighten-4 grey-text">
+                  <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
+                  <div>2nd September, 2am</div>
+               </div>
             </div>
          </div>
+      )
+   }
+   return (
+      <div className="containter center">
+         <p>Loading project....</p>
       </div>
    )
 }
 
-export default ProjectDetails
+const mapStateToProps = (state, ownProps) => { //ownProps used to get props from component
+   console.log('projectDetails mapstate', state);
+
+   const id = ownProps.match.params.id
+   const projects = state.firestore.data.projects
+   const project = projects ? projects[id] : null
+   return {
+      project: project
+   }
+}
+
+export default compose(
+   connect(mapStateToProps),
+   firestoreConnect(() => ['projects']), //sync 'projects' collection from Firestore into redux
+   //alt way
+   // firestoreConnect([
+   //    { collection: 'projects' }
+   // ])
+)(ProjectDetails)
